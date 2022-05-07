831b1551b9e7edfb9355d371fe6cd3d4c324b44b
Rob Weltman <robw@worldspot.com>
2022-05-06 17:20:38 -1000

    Portable db config

--- db/config.php
+++ db/config.php
@@ -1,20 +1,11 @@
 <?php
 $home ="/" . basename(__DIR__);
-/*
-$dbconfig = [
-    'servername' => "localhost",
-    'socket' => "/opt/local/var/run/mariadb-10.7/mysqld.sock",
-    'username' => "root",
-    'password' => "",
-    'db' => "laana",
-];
-*/
+
 $dbconfig = [
     'servername' => "worldspot.com",
-    //'socket' => "/opt/local/var/run/mariadb-10.7/mysqld.sock",
+    'socket' => "/opt/local/var/run/mariadb-10.7/mysqld.sock",
     'username' => "laana",
     'password' => '0$o7Z&93',
     'db' => "laana",
 ];
-
 ?>
