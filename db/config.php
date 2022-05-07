4902bbb814f51421d5f03e316678cc51d5dc0811
Rob Weltman <robw@worldspot.com>
2022-05-06 16:14:54 -1000

    Use remote credentials

--- db/config.php
+++ db/config.php
@@ -1,19 +1,19 @@
 <?php
 $home ="/" . basename(__DIR__);
-$hostname = $_SERVER['SERVER_NAME'] ?: "mauihikes.org";
-$baseurl = "https://$hostname$home";
-
-$adminemail = "info@$hostname";
-$orgname = "Sierra Club Maui Hikes";
-$orgemail = "info@mauisierraclub.org ";
-
-$erroremail = "robw@worldspot.com";
-
+/*
 $dbconfig = [
     'servername' => "localhost",
     'socket' => "/opt/local/var/run/mariadb-10.7/mysqld.sock",
     'username' => "root",
     'password' => "",
+    'db' => "laana",
+];
+*/
+$dbconfig = [
+    'servername' => "worldspot.com",
+    //'socket' => "/opt/local/var/run/mariadb-10.7/mysqld.sock",
+    'username' => "laana",
+    'password' => '0$o7Z&93',
     'db' => "laana",
 ];
 
