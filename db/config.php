458b5f03343e83605bae1070425870c1fd011d8f
Rob Weltman <robw@worldspot.com>
2022-05-12 21:37:13 -1000

    Remove password

--- db/config.php
+++ db/config.php
@@ -5,7 +5,7 @@ $dbconfig = [
     'servername' => "worldspot.com",
     'socket' => "/opt/local/var/run/mariadb-10.7/mysqld.sock",
     'username' => "laana",
-    'password' => '0$o7Z&93',
+    'password' => '',
     'db' => "laana",
 ];
 ?>
