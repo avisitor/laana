0db381688767fb93376e9c2b93ff2f6bed49192a
Rob Weltman <robw@worldspot.com>
2023-01-13 15:54:10 -1000

    Simplified highlighting that also works for exact phrase matches

--- ops/getPageHtml.php
+++ ops/getPageHtml.php
@@ -11,33 +11,18 @@ debuglog( $_REQUEST );
 $laana = new Laana();
 $output = "";
 if( $word ) {
-    $normalizedWord = normalizeString( $word );
+    $target = ($pattern == 'exact') ? $word : normalizeString( $word );
+    $targetwords = preg_split( "/[\s]+/",  $target );
+    $tw = ($pattern == 'exact') ? $target : implode( '|', $targetwords );
+    $pat = "/(" . $tw . ")/";
+    $repl = '<span class="match">$1</span>';
     $rows = $laana->getSentences( $word, $pattern, $page );
     foreach( $rows as $row ) {
         $source = $row['sourcename'];
         $authors = $row['authors'];
         $link = $row['link'];
         $sourcelink = "<a href='$link' target='_blank'>$source</a>";
-        $sentence = $row['hawaiiantext'];
-        $result = "";
-        $target = ($pattern == 'exact') ? $word : $normalizedWord;
-        $tw = $target;
-        $targetwords = preg_split( "/[\s]+/",  $target );
-        $words = preg_split( "/[\s,]+/",  $sentence );
-        foreach( $words as $w ) {
-            //$normalized = ($word == $normalizedWord) ? normalizeString( $w ) : $w;
-            $normalized = normalizeString( $w );
-            foreach( $targetwords as $tw ) {
-                $sourceword = ( preg_match( "/[ōīēūāŌĪĒŪĀ‘ʻ]/", $tw ) ) ? $w : $normalized;
-                //error_log( "index.php: comparing $tw to $sourceword" );
-                if( !strcasecmp( $tw, $sourceword ) ) {
-                    //error_log( "index.php: matched $tw to $sourceword" );
-                    $w = '<span class="match">' . $w . '</span>';
-                }
-            }
-            $result .= $w . ' ';
-        }
-        $sentence = $result;
+        $sentence = preg_replace($pat, $repl, $row['hawaiiantext'] );
         $translate = "https://translate.google.com/?sl=auto&tl=en&op=translate&text=" .
                    $row['hawaiiantext'];
         $output .= <<<EOF
