adffa271c3c1a3b5e8b9978a7054e7e4a05996c2
Rob Weltman <robw@worldspot.com>
2023-10-28 20:24:24 -1000

    Options to see sentence in context

--- ops/getPageHtml.php
+++ ops/getPageHtml.php
@@ -11,27 +11,48 @@ debuglog( $_REQUEST );
 $laana = new Laana();
 $output = "";
 if( $word ) {
+    $replace = [
+        "/a|ā|Ā/" => "[aĀā]",
+        //        "/ā/" => "[aĀā]",
+        //        "/Ā/" => "[aĀā]",
+    ];
+    $repl = "<span>$1</span>";
     $target = ($pattern == 'exact') ? $word : normalizeString( $word );
+    $target = normalizeString( $word );
     $targetwords = preg_split( "/[\s]+/",  $target );
     $tw = ($pattern == 'exact') ? $target : implode( '|', $targetwords );
-    $pat = "/(" . $tw . ")/";
+    $expanded = "/(" . preg_replace( array_keys( $replace ),
+                                     array_values( $replace ),
+                                     $tw ) . ")/ui";
+    
+    $pat = "/(" . $tw . ")/ui";
+    $pat = $expanded;
     $repl = '<span class="match">$1</span>';
     $rows = $laana->getSentences( $word, $pattern, $page );
     foreach( $rows as $row ) {
         $source = $row['sourcename'];
+        $sentenceid = $row['sentenceid'];
         $authors = $row['authors'];
         $link = $row['link'];
         $sourcelink = "<a href='$link' target='_blank'>$source</a>";
+        $idlink = "<a href='context?id=$sentenceid&raw' target='_blank'>Context</a>";
+        $simplified = "<a href='context?id=$sentenceid' target='_blank'>Simplified</a>";
         $sentence = preg_replace($pat, $repl, $row['hawaiiantext'] );
+        error_log( "pat: $pat, repl: $repl");
         $translate = "https://translate.google.com/?sl=auto&tl=en&op=translate&text=" .
-                   $row['hawaiiantext'];
+                     $row['hawaiiantext'];
         $output .= <<<EOF
                   
             <div class="hawaiiansentence">
               <p class='title'>$sentence</p>
-              <p class='source'>$sourcelink</p>
-              <p class='source'>$authors</p>
-              <p class='source'><a target='_blank' href='$translate'>translate</a></p>
+              <p style='font-size:0.6em;margin-bottom:0;'>
+              <span class='hawaiiansentence source'>$sourcelink</span>&nbsp;&nbsp;
+              </p><p style='font-size:0.5em;margin-top:0.1em;'>
+              <span class='source'>$idlink</span>&nbsp;&nbsp;
+              <span class='source'>$simplified</span>&nbsp;&nbsp;
+              <span class='source'>$authors</span>&nbsp;&nbsp;
+              <span class='source'><a target='_blank' href='$translate'>translate</a>
+              </p>
             </div>
             <hr>
  EOF;
