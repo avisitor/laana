8f9db870b700dc4b3531575b0fe436b0192e5a33
Rob Weltman <robw@worldspot.com>
2023-11-05 20:09:07 -1000

    Added creation timestamps, made hawaiianText of type text

--- createtables.sql
+++ createtables.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `sentences` (
   `sentenceID` int(11) NOT NULL AUTO_INCREMENT,
   `sourceID` int(11) NOT NULL,
-  `hawaiianText` varchar(255) NOT NULL,
+  `hawaiianText` text,
   `englishText` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (sourceID, hawaiianText),
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (sourceID, hawaiianText(100)),
   UNIQUE KEY `sentenceID` (`sentenceID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27906 DEFAULT CHARSET=utf8;
 
@@ -14,6 +15,10 @@ CREATE TABLE `sources` (
   `link` text DEFAULT NULL,
   `start` int(11) DEFAULT NULL,
   `end` int(11) DEFAULT NULL,
+  groupname varchar(20)b,
+  title varchar(100),
+  date DATE,
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`sourceID`),
   UNIQUE KEY `sourceID` (`sourceID`),
   UNIQUE KEY `sourceName` (`sourceName`) USING HASH
@@ -29,6 +34,7 @@ CREATE TABLE `contents` (
   `sourceID` int(11) NOT NULL,
   `html` text,
   `text` text,
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`sourceID`),
   UNIQUE KEY `sourceID` (`sourceID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27906 DEFAULT CHARSET=utf8;
