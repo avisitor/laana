7814c3b95671083ba3eb6382340dec1750f8ff7f
Rob Weltman <robw@worldspot.com>
2023-11-05 20:15:56 -1000

    Added links to both html and simplified versions of documents from the DB

--- rawpages.php
+++ rawpages.php
@@ -1,9 +1,10 @@
 <?php
 include 'db/parsehtml.php';
 
-$sql = "select sources.sourceid,sources.sourcename from sources,contents where sources.sourceid=contents.sourceid and not html is null order by sources.sourcename";
+$sql = "select sources.sourceid,sources.sourcename,sources.link from sources,contents where sources.sourceid=contents.sourceid and not html is null order by sources.sourcename";
 $db = new DB();
 $rows = $db->getDBRows( $sql );
+$count = sizeof( $rows );
 ?>
 <html>
     <head>
@@ -16,18 +17,25 @@ $rows = $db->getDBRows( $sql );
         </style>
     </head> 
     <body>
-        <h2>Ulukau documents</h2>
+        <h2>Stored Documents</h2>
+        <h5><?=$count?> files</h5>
         <ul>
 
-<?php
-foreach( $rows as $row ) {
-    $title = $row['sourcename'];
-    $sourceid = $row['sourceid'];
-    $link = "rawpage?id=$sourceid";
-    $item = "<li><a href='$link'>$title</a></li>";
-    echo "$item\n";
-}
-?>
+            <?php
+            foreach( $rows as $row ) {
+                $title = $row['sourcename'];
+                $sourceid = $row['sourceid'];
+                $link = "rawpage?id=$sourceid";
+                $link2 = $link . "&simplified";
+                $source = $row['link'];
+                $item = "<li>$title<br/>" .
+                        "<a href='$link' target='_blank'>HTML</a>&nbsp;|&nbsp;" .
+                        "<a href='$link2' target='_blank'>Simplified</a>&nbsp;|&nbsp;" .
+                        "<a href='$source' target='_blank'>Source</a>" .
+                        "</li>";
+                echo "$item\n";
+            }
+            ?>
 </ul>
 </body>
 </html>
