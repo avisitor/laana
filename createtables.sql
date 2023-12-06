9b3cad19510fc529d6f7ea7fd9e8a62f2df0f57f
Rob Weltman <robw@worldspot.com>
2023-12-05 14:51:01 -1000

    Functionally complete snapshot before switching to natural language searches

--- createtables.sql
+++ createtables.sql
@@ -4,6 +4,7 @@ CREATE TABLE `sentences` (
   `hawaiianText` text,
   `englishText` varchar(255) DEFAULT NULL,
   created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FULLTEXT (hawaiianText),
   PRIMARY KEY (sourceID, hawaiianText(100)),
   UNIQUE KEY `sentenceID` (`sentenceID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27906 DEFAULT CHARSET=utf8;
@@ -30,6 +31,12 @@ CREATE TABLE `searchstats` (
   UNIQUE KEY `searchterm` (`searchterm`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `stats` (
+  `name` varchar(255) NOT NULL,
+  `value` int(11) NOT NULL,
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `contents` (
   `sourceID` int(11) NOT NULL,
   `html` text,
@@ -38,4 +45,27 @@ CREATE TABLE `contents` (
   PRIMARY KEY (`sourceID`),
   UNIQUE KEY `sourceID` (`sourceID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27906 DEFAULT CHARSET=utf8;
+
+DELIMITER //
+CREATE TRIGGER insert_sentences AFTER INSERT ON sentences
+FOR EACH ROW UPDATE stats SET value = value + 1 where name = 'sentences';
+
+DELIMITER //
+CREATE TRIGGER update_sentences AFTER INSERT ON sentences
+FOR EACH ROW
+update sentences set simplified = replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(hawaiianText,'ō','o'),'ī','i'),'ē','e'),'ū','u'),'ā','a'),'Ō','O'),'Ī','I'),'Ē','E'),'Ū','U'),'Ā','A'),'‘',''),'ʻ','') where sentenceid = NEW.sentenceid;
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER delete_sentences AFTER DELETE ON sentences
+FOR EACH ROW UPDATE stats SET value = value - 1 where name = 'sentences';
+//
+CREATE TRIGGER insert_sources AFTER INSERT ON sources
+FOR EACH ROW UPDATE stats SET value = value + 1 where name = 'sources';
+//
+CREATE TRIGGER delete_sources AFTER DELETE ON sources
+FOR EACH ROW UPDATE stats SET value = value - 1 where name = 'sources';
+//
+DELIMITER ;
 
