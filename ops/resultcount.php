6839858a253985f397b8811a0a70b3a85609c15d
Rob Weltman <robw@worldspot.com>
2024-02-26 17:09:15 -1000

    Do not urlencode the search term for regex searches

--- ops/resultcount.php
+++ ops/resultcount.php
@@ -3,11 +3,6 @@ header('Content-type: text/plain');
 include '../db/funcs.php';
 $word = isset($_GET['search']) ? $_GET['search'] : "";
 $pattern = isset($_GET['searchpattern']) ? $_GET['searchpattern'] : "any";
-if( $word ) {
-    if( $pattern == 'regex' ) {
-        $word = urlencode( $word );
-    }
-}
 $nodiacriticals = ( isset( $_REQUEST['nodiacriticals'] ) && $_REQUEST['nodiacriticals'] == 1 ) ? 1 : 0;
 $options = [];
 if( $nodiacriticals ) {
