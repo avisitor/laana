32e9e37564bc5e38d586a8d2f3e11454569cf96d
Rob Weltman <robw@worldspot.com>
2025-05-04 15:23:58 -1000

    Added options for local, minsource, maxsource

--- scripts/savedocument.php
+++ scripts/savedocument.php
@@ -4,23 +4,32 @@ include 'saveFuncs.php';
 $longopts = [
     "force",
     "debug",
+    "local",
     'sourceid:',
+    'minsource:',
+    'maxsource:',
     'parser:',
 ];
 $args = getopt( "", $longopts );
 $force = isset( $args['force'] ) ? true : false;
 $debug = isset( $args['debug'] ) ? true : false;
+$local = isset( $args['local'] ) ? true : false;
 $sourceid = isset( $args['sourceid'] ) ? $args['sourceid'] : 0;
+$minsourceid = isset( $args['minsource'] ) ? $args['minsource'] : 0;
+$maxsourceid = isset( $args['maxsource'] ) ? $args['maxsource'] : PHP_INT_MAX;
 $parserkey = isset( $args['parser'] ) ? $args['parser'] : '';
 
 // For running in the debugger without command line arguments
 $parserkey = ($parserkey) ? $parserkey : "kauakukalahale";
-$sourceid = ($sourceid) ? $sourceid : 24308;
+#$sourceid = ($sourceid) ? $sourceid : 24308;
 
 $options = [
     'force' => $force,
     'debug' => $debug,
+    'local' => $local,
     'sourceid' => $sourceid,
+    'minsourceid' => $minsourceid,
+    'maxsourceid' => $maxsourceid,
 ];
 
 $parser = $parsermap[$parserkey];
