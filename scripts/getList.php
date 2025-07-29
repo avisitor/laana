0404d6ac4373be19699570be64b7d143a2468365
Rob Weltman <robw@worldspot.com>
2025-07-28 17:39:49 -1000

    Changed the script name

---
+++ scripts/getList.php
@@ -0,0 +1,26 @@
+<?php
+include_once __DIR__ . '/../scripts/saveFuncs.php';
+
+$longopts = [
+    "debug",
+    'parser:',
+];
+$args = getopt( "", $longopts );
+$debug = isset( $args['debug'] ) ? true : false;
+$parserkey = isset( $args['parser'] ) ? $args['parser'] : '';
+$options = [
+    'debug' => $debug,
+];
+
+$saveManager = new SaveManager( $options );
+$parser = $saveManager->getParser($parserkey);
+if( !$parser ) {
+    $values = $saveManager->getParserKeys();
+    echo "Specify a parser: $values\n";
+    echo "testList [--debug] --parser=parsername\n";
+} else {
+    setDebug( $debug );
+    $docs = $parser->getDocumentList();
+    echo( var_export( $docs, true ) . "\n" );
+}
+?>
