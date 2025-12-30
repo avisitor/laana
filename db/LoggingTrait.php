6769144bdc03497e31a3511750c30ba1da75f4f0
Rob Weltman <robw@worldspot.com>
2025-12-29 17:56:24 -1000

    Added more output options

--- db/LoggingTrait.php
+++ db/LoggingTrait.php
@@ -18,25 +18,68 @@
 
 trait LoggingTrait {
     /**
-     * Format a log message with context
+     * Format a log message with funcName, using json if not a string
      */
-    protected function formatLog($obj, $prefix = "") {
-        if ($prefix && !is_string($prefix)) {
-            $prefix = json_encode($prefix);
+    protected function formatLog($msg, $prefix = "") {
+        if ($msg && !is_string($msg)) {
+            $msg = json_encode($msg);
+        }
+        $context = "";
+        if ($this->logName) {
+            $context = $this->logName . ":";
         }
         if ($this->funcName) {
-            $func = $this->logName . ":" . $this->funcName;
-            $prefix = ($prefix) ? "$func:$prefix" : $func;
+            $context .= $this->funcName . ":";
         }
-        return $prefix;
+        if( $prefix ) {
+            $context .= $prefix;
+        }
+        if( $msg ) {
+            if( $context ) {
+                $msg = $context . ":" . $msg;
+            }
+        } else {
+            $msg = $context;
+        }
+        return $msg;
+    }
+    
+    /**
+     * Format a log message with timestamp and script name
+     */
+    protected function formatLogMessage($msg, $intro = "") {
+        if ($msg && !is_string($msg)) {
+            $msg = var_export($msg, true);
+        }
+        $defaultTimezone = 'Pacific/Honolulu';
+        $now = new DateTimeImmutable("now", new DateTimeZone($defaultTimezone));
+        $now = $now->format('Y-m-d H:i:s');
+        $out = "$now " . $_SERVER['SCRIPT_NAME'];
+        if ($intro) {
+            $out .= " $intro:";
+        }
+        return "$out $msg";
     }
     
     /**
      * Log a message with context
      */
     public function log($obj, $prefix = "") {
-        $prefix = $this->formatLog($obj, $prefix);
-        $this->debuglog($obj, $prefix);
+        $prefix = $this->formatLog( $obj, $prefix );
+        $this->debuglog( $prefix );
+    }
+
+    /*
+     * Print a message to stdout, using var_export if not a string
+     */
+    public function print($msg, $prefix = "") {
+        if ($msg && !is_string($msg)) {
+            $msg = var_export($msg, true);
+        }
+        if( $prefix ) {
+            $msg = "$prefix: $msg";
+        }
+        $this->outputLine($msg);
     }
     
     /**
@@ -51,8 +94,7 @@ trait LoggingTrait {
     
     public function verbosePrint($obj, $prefix = "") {
         if ($this->verbose) {
-            $text = $this->formatLog($obj, $prefix);
-            $this->printObject($obj, $text);
+            $this->print($obj, $prefix);
         }
     }
     
@@ -65,7 +107,15 @@ trait LoggingTrait {
     }
 
     /**
-     * Output a message to stdout (can be suppressed by setting $verbose to false)
+     * Set global debug flag (for backward compatibility with legacy code)
+     */
+    protected function setGlobalDebug($debug) {
+        global $debugFlag;
+        $debugFlag = $debug;
+    }
+
+    /**
+     * Output a message to stdout
      */
     public function output($message) {
         echo $message;
@@ -79,73 +129,20 @@ trait LoggingTrait {
     }
 
     /**
+     * Print object to stdout with formatting (for debug output only)
+     */
+    protected function printObject($obj, $intro = '') {
+        echo( $this->formatLogMessage( $obj, $intro ) );
+    }
+
+    /**
      * Core logging implementation - logs a message to error log file
-     * Self-contained - does not call global functions
      */
     protected function debuglog($msg, $intro = "") {
         if (!defined('PHPUNIT_RUNNING') || !PHPUNIT_RUNNING) {
-            // Format the message inline without calling global formatLogMessage
-            if (is_object($msg) || is_array($msg)) {
-                $msg = var_export($msg, true);
-            }
-            $defaultTimezone = 'Pacific/Honolulu';
-            $now = new DateTimeImmutable("now", new DateTimeZone($defaultTimezone));
-            $now = $now->format('Y-m-d H:i:s');
-            $out = "$now " . ($_SERVER['SCRIPT_NAME'] ?? 'CLI');
-            if ($intro) {
-                $out .= " $intro:";
-            }
-            $formattedMsg = "$out $msg";
-            
             // Log to error log file (configured in php.ini)
-            error_log($formattedMsg);
+            error_log( $this->formatLogMessage( $msg, $intro ) );
         }
         return;
-    }
-
-    /**
-     * Print object to stdout with formatting (for debug output only)
-     * Self-contained - does not call global functions
-     */
-    protected function printObject($obj, $intro = '') {
-        // Format the message inline
-        if (is_object($obj) || is_array($obj)) {
-            $msg = var_export($obj, true);
-        } else {
-            $msg = $obj;
-        }
-        $defaultTimezone = 'Pacific/Honolulu';
-        $now = new DateTimeImmutable("now", new DateTimeZone($defaultTimezone));
-        $now = $now->format('Y-m-d H:i:s');
-        $out = "$now " . ($_SERVER['SCRIPT_NAME'] ?? 'CLI');
-        if ($intro) {
-            $out .= " $intro:";
-        }
-        echo "$out $msg\n";
-    }
-
-    /**
-     * Format a log message with timestamp and script name
-     */
-    protected function formatLogMessage($msg, $intro = "") {
-        if (is_object($msg) || is_array($msg)) {
-            $msg = var_export($msg, true);
-        }
-        $defaultTimezone = 'Pacific/Honolulu';
-        $now = new DateTimeImmutable("now", new DateTimeZone($defaultTimezone));
-        $now = $now->format('Y-m-d H:i:s');
-        $out = "$now " . $_SERVER['SCRIPT_NAME'];
-        if ($intro) {
-            $out .= " $intro:";
-        }
-        return "$out $msg";
-    }
-
-    /**
-     * Set global debug flag (for backward compatibility with legacy code)
-     */
-    protected function setGlobalDebug($debug) {
-        global $debugFlag;
-        $debugFlag = $debug;
     }
 }
