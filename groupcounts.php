5c46331ba92fb0ca667186e1c47530c8b3e6138e
Rob Weltman <robw@worldspot.com>
2026-01-04 18:30:58 -1000

    Pass provider name to getcounts

--- groupcounts.php
+++ groupcounts.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/lib/provider.php';
-$provider = getProvider();
-
+$providerName = isset($_REQUEST['provider']) ? $_REQUEST['provider'] : getProvider()->getName();
+$provider = getProvider( $providerName );
 $rows = $provider->getSourceGroupCounts();
+//error_log( "groupcounts from $providerName: " . var_export( $rows, true ) );
 echo json_encode( $rows );
 ?>
